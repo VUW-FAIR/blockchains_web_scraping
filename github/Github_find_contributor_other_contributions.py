c3e6a4e7298dc2e145ac63b9cb21b241e0806c51
dknz2008 <dylan.kumar@icloud.com>
2018-02-14 10:26:04 +1300

    adding comments

--- github/Github_find_contributor_other_contributions.py
+++ github/Github_find_contributor_other_contributions.py
@@ -6,7 +6,12 @@ import os
 contributors = {}
 
 '''
-This script goes over all the files in the github folder and finds which repos the contributor has contributed to. 
+This script goes over all the files in the github folder and finds which repos the each user has contributed to. 
+'''
+
+
+'''
+Parse the contributors json file and add to dictonary of contributors
 '''
 def parse_contributors_page(name_of_repo, file_path):
 
@@ -32,20 +37,25 @@ def parse_contributors_page(name_of_repo, file_path):
 def iterate_through_folders():
     rootdir = '.\\GitHubScraping'
 
+    #go over each file
     for subdir, dirs, files in os.walk(rootdir):
         for file in files:
             path = os.path.join(subdir).replace(str(os.path.join(subdir).split("\\", -1)[-1]), "")
             print(path)
-
+            
             contibutor_path = path + "all_contributors\contributors_page_"
+            
+            #if that string is in our path directory we're at the contributors json file
             if(contibutor_path in os.path.join(subdir, file)):
-                print('a0')
                 print(os.path.join(subdir, file))
+                #parse the json file
                 parse_contributors_page(name_of_repo=os.path.join(subdir, file), file_path=os.path.join(subdir, file))
 
     write_to_csv()
 
-
+'''
+Write contributors dictionary to csv
+'''
 def write_to_csv():
     with open('github_contributors_output.csv', 'w') as csv_file:
         writer = csv.writer(csv_file)
