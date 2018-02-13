f012e3ed73822d238bd5dbea316c09e7c5d47fb4
dknz2008 <dylan.kumar@icloud.com>
2018-02-13 15:54:47 +1300

    Add files via upload

--- Graphs/term_frequency_for_year.py
+++ Graphs/term_frequency_for_year.py
@@ -2,29 +2,45 @@ import collections
 import csv
 import glob
 from operator import itemgetter
-
+from collections import Counter
 import matplotlib.pyplot as plt
 import sys
+from dateutil.parser import parse
 
 unwanted_chars = ".,-_\n"
+
+## SET YOU YEAR HERE:
+year = 2018
 
 def main():
 
     total_freq = {}
-
+    dates = []
     csv_text = ""
 
-    for file in glob.glob("bitcoin_talk_forum/*.csv"):
+    for file in glob.glob("forumbitcoincom/*.csv"):
         print(file)
         with open(file, encoding="utf8") as f:
             reader = csv.reader(x.replace('\0', '') for x in f)
-            csv_list = list(reader)
-        print(csv_list)
+            #csv_list = list(reader)
 
-        for elements in csv_list:
-            if(len(elements) > 0):
-                print(elements[2])
-                csv_text += " " + elements[2]
+            for row in reader:
+
+                try:
+                    print(row[2])
+                    b = parse(row[2])
+                    print(b)
+                    dates.append(b.date())  # only take the date (not datetime as we don't need the time)
+
+                    if(b.date().year == year):
+                        if (len(row) > 0):
+                            #print(row[3])
+                            csv_text += " " + row[3]
+
+                except:
+                    print("couldn't parse date")
+
+        #print(csv_list)
 
     text = csv_text
 
@@ -48,9 +64,9 @@ def main():
         print(key, value)
         sorted_top[key] = value
         i+=1
+        #CHOOSE THE NUMBER OF WORDS YOU WANT HERE:
         if i > 100:
             break
-
 
     plotGraph(sorted_top)
 
@@ -62,12 +78,12 @@ def countOccurrences(text):
     count = 0
     for word in words:
         word = word.strip(unwanted_chars)
-        print(word)
+        #print(word)
         if word not in freq:
             freq[word] = 0
         freq[word] += 1
         count = count + 1
-    print(freq)
+    #print(freq)
 
     return [freq, count]
 
@@ -76,10 +92,11 @@ def plotGraph(dict):
     plt.bar(range(len(dict)), dict.values(), align='center')
     plt.xticks(range(len(dict)), dict.keys(), rotation=90)
 
-    plt.xlabel('Words')
-    plt.ylabel('Number of occurrences')
-    plt.title('Top words for bitcointalk Forum\n')
-
+    plt.xlabel('word')
+    plt.ylabel('number of occurrences')
+    plt.title('Occurrences for words on forumbitcoincom: \n' + str(year))
+    #plt.savefig( str(year) + ".png")
     plt.show()
+
 
 main()
